5333712363ff34133caa07b2c01324691a212414
RudiYare <rezan20031@gmail.com>
2022-04-10 16:47:58 +0300

    Добавлены изменения в поиске

--- MainForm/MainForm/BD/PairSearch.cs
+++ MainForm/MainForm/BD/PairSearch.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace bd
 {
+    // Используется для поиска
     internal class PairSearch
     {
         public string filter;
         public string textSearch;
         public PairSearch(string f, string t)
         {
+            
             filter = f;
             textSearch = t;
         }
