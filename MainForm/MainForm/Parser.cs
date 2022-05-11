0fd12bc48c7c24e151b3387bb55450d74c3d18db
DarkAngelPro <volodya-korzhov@mail.ru>
2022-05-11 12:23:21 +0300

    Добавлен файл класса для парсера, подключены основные библиотеки для дальнейшей работы

---
+++ MainForm/MainForm/Parser.cs
@@ -0,0 +1,14 @@
+﻿using bd;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using AngleSharp;
+
+namespace MainForm
+{
+    //Класс парсера
+    public class Parser
+    {
+    }
+}
